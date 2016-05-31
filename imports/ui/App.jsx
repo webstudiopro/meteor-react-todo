43ff6fc68754ea0f66bc14c83d19c37b2be4905f
Fret Benny <info@webstudiopro.be>
2016-05-31 08:37:03 +0200

    Todo React App

---
+++ imports/ui/App.jsx
@@ -0,0 +1,36 @@
+import React, {Component, PropTypes} from 'react';
+import { createContainer } from 'meteor/react-meteor-data';
+import { Tasks } from '../api/tasks.js';
+import Task from './Task.jsx';
+
+class App extends Component {
+    renderTasks() {
+        return this.props.tasks.map((task) => (
+            <Task key={task._id} task={task} />
+        ));
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <header>
+                    <h1>Todo List</h1>
+                </header>
+
+                <ul>
+                    {this.renderTasks()}
+                </ul>
+            </div>
+        );
+    }
+}
+
+App.propTypes = {
+    tasks: PropTypes.array.isRequired
+};
+
+export default createContainer(() => {
+    return {
+        tasks: Tasks.find({}).fetch(),
+    };
+}, App);
